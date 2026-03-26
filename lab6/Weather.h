eaa97204da458ed5acd517df510b7f18c566b9c3
darius-andrei15 <mdarius110@gmail.com>
2026-03-26 18:03:11 +0200

    Create Weather.h

---
+++ lab6/Weather.h
@@ -0,0 +1,17 @@
+//
+// Created by Darius on 3/25/2026.
+//
+
+#ifndef LAB6_WEATHER_H
+#define LAB6_WEATHER_H
+#pragma once
+
+
+enum class Weather {
+    Rain,
+    Sunny,
+    Snow
+};
+
+
+#endif //LAB6_WEATHER_H
