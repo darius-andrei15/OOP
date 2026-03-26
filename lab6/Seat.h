fa03dce77725fec6b26ac990f35db5c57254c1e8
darius-andrei15 <mdarius110@gmail.com>
2026-03-26 18:10:25 +0200

    Create Seat.h

---
+++ lab6/Seat.h
@@ -0,0 +1,17 @@
+//
+// Created by Darius on 3/25/2026.
+//
+
+#ifndef LAB6_SEAT_H
+#define LAB6_SEAT_H
+#include "Car.h"
+
+class Seat: public Car {
+public:
+    double FuelCapacity() const override;
+    double FuelCost() const override;
+    double AverageSpeed(Weather weather) const override;
+    const char* Name() const override;
+};
+
+#endif //LAB6_SEAT_H
